dd3b0f3e204d2de458feafb0223a68856365d4dc
mariam <mariam20546@gmail.com>
2024-03-26 18:09:02 -0400

    added a new file

---
+++ assignment.js
@@ -0,0 +1,51 @@
+let number=("enter a number")
+let positiveNumber="positive"
+let negativeNumber="negative"
+let zeroNumber="zero"
+if (number>0){
+ console.log(positiveNumber);
+}else if(number<0){
+console.log(negativeNumber);
+}else {
+console.log(zeroNumber)
+}
+
+
+let number1 =("enter a number")
+let evenNumber =("even")
+let oddNumber =("odd")
+if (number1%2==0){
+ console.log(evenNumber)
+}else{
+  console.log(oddNumber)
+}
+
+
+
+let age =("31")
+if (age%5==0){
+  console.log("multiple of 5")
+} else if(age%3==0){
+ console.log("multiple of 3")
+}else
+ console.log("Neither")
+
+
+let year=("enter a year")
+if (year>=1901 && year<=2000){
+  console.log("20th century")
+}else if(year>=2001 && year<=2100){
+  console.log("21st century")
+}else if(year>=2101 && year<=2200)
+ console.log("22nd century")
+
+
+let value=("enter a value")
+ if (value<10)
+ console.log(0);
+ else if(value==10 && value<=50){
+ console.log(1)
+  }else if(value==51 && value<=100){
+   console.log(2)
+  }else
+    console.log(3)
