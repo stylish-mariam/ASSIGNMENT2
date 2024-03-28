8e45df9a515800c7d136b3a4e1c1d6cdc79ccea2
mariam <mariam20546@gmail.com>
2024-03-27 20:32:47 -0400

    added a new folder and a new file

---
+++ classwork.js/classwork.js
@@ -0,0 +1,24 @@
+let score = ""
+if(score >=30){
+  console.log("passed")
+  if(score>=70){
+    console.log("A")
+  }else if(score>=50 && score<=69){
+   console.log("B")
+ }else if(score>=30 && score<=49){
+   console.log("C")}
+ }else
+{
+   console.log("failed")
+  }
+
+
+ let age ="60"
+ if(age<18){
+   console.log("I am a child")
+ }if(age>=18 && age<=64){
+   console.log("I am an adult")
+ }else if(age>=65){
+     console.log("I am an old geezer")
+ }
+ 
